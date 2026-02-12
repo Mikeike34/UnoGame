3699075f8e4ce46355781e2943852aaba4b0eb5c
Mikeike34 <michaeljcantone@gmail.com>
2026-02-11 16:17:50 -0800

    config for deploy

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'socket.io-client']
+        }
+      }
+    }
+  }
 })
